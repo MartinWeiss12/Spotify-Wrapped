345b836cf0094a268c6b69e25472fb80d6ecbbfb
Martin Weiss <martinweiss1255@gmail.com>
2023-07-12 22:07:56 -0400

    Updated code - 7/12/2023

--- spotifyWrapped.py
+++ spotifyWrapped.py
@@ -1,152 +1,119 @@
+import os
 import pytz
 import time
-import os, os.path
 import pandas as pd
 from datetime import datetime
-from IPython.display import display
 
-start_time = datetime.now()
-start_time = start_time.strftime('%H:%M:%S')
-print('Start Time:', start_time)
-startTimerSec = time.time()
-path = r'' #path of your file with all endsong json files
-outputPath = r'' #path where to output excel files
-data = []
-files = os.listdir(path)
-fileCount = 0
-for file in files:
-	if file.endswith('json'):
-		df = pd.read_json(path + '/endsong_' + str(fileCount) + '.json')
-		fileCount = fileCount + 1
-		data.append(df)
-data = pd.concat(data)
-data = data[data.ms_played >= 30000] #streams longer than 30 seconds
-data.reset_index(drop = True, inplace = True)
-for stream in data.index:
-	if(data['spotify_track_uri'][stream] is None):
-		data.drop([stream], axis = 0, inplace = True)
+# path of your folder with all endsong json files
+path = ''
+# path of where the excel files will be saved to
+output_path = ''
 
-est = pytz.timezone('US/Eastern')
-utc = pytz.utc
-fmt = '%Y-%m-%d %H:%M:%S'
-for ind in data.index:
-	date = (data['ts'][ind])
-	holdTS = datetime(int(date[0:4]), int(date[5:7]),
-		int(date[8:10]), int(date[11:13]),
-		int(date[14:16]), int(date[17:19]), tzinfo = utc)
-	estTS = holdTS.astimezone(est).strftime(fmt)
-	data.at[ind, 'ts'] = estTS
-dateList = data['ts'].tolist()
-date = '2020-01-01 00:00:00'
-#data = data[data.ts >= date] #streams from after Jan 01, 2020 12:00:00 AM
-#data = data[data.ts <= date] #streams from before Jan 01, 2020 12:00:00 AM
-msPlayedList = (data['ms_played']).tolist()
-print('Minutes Played:', round(sum(msPlayedList)/60000))
-data.rename(columns = {'ts':'Timestamp', 'master_metadata_track_name':'Track',
-	'master_metadata_album_artist_name':'Artist', 'master_metadata_album_album_name': 'Album', 'spotify_track_uri':'URI'}, inplace = True)
-data = data[['Timestamp', 'Track', 'Artist', 'Album', 'URI']]
-data = (data.sort_values(by = 'Timestamp')).reset_index(drop = True)
-for ind in data.index:
-	trackUri = (data['URI'][ind])
-	holdTrackUri = trackUri.replace('spotify:track:', '')
-	data.at[ind, 'URI'] = holdTrackUri
-uriList = data['URI'].tolist()
-data.to_excel(f'{outputPath}/spotify_streaming_log.xlsx', index = False)
+files = [os.path.join(path, f) for f in os.listdir(path) if os.path.isfile(os.path.join(path, f)) and f.endswith('.json')]
+data = pd.concat([pd.read_json(file) for file in files])
 
-tsList = data['Timestamp'].tolist()
-trackList = data['Track'].tolist()
+# filter out streams less than 30 seconds
+filtered_data = data[data['ms_played'] >= 30000].reset_index(drop=True)
+# drop rows that do not have a URI
+filtered_data = filtered_data[filtered_data['spotify_track_uri'].notnull()]
+
+timezone = pytz.timezone('US/Eastern')
+datetime.now(tz=timezone)
+
+filtered_data['ts'] = pd.to_datetime(filtered_data['ts'], utc=True)
+filtered_data['ts'] = filtered_data['ts'].dt.tz_convert(timezone)
+filtered_data['ts'] = filtered_data['ts'].dt.strftime('%Y-%m-%d %H:%M:%S')
+
+# filter FROM a date
+#start_date = '2022-12-31'
+#filtered_data = filtered_data[filtered_data['ts'] >= start_date]
+
+# filter TO a date
+#end_date = '2022-09-26'
+#filtered_data = filtered_data[filtered_data['ts'] <= end_date]
+
+streaming_log = filtered_data[['ts', 'master_metadata_track_name', 'master_metadata_album_album_name', 'master_metadata_album_artist_name', 'spotify_track_uri']]
+streaming_log.columns = ['Timestamp', 'Track', 'Album', 'Artist', 'URI']
+streaming_log = streaming_log.sort_values('Timestamp')
+
+ts_list = streaming_log['Timestamp'].tolist()
+track_list = streaming_log['Track'].tolist()
+uri_list = streaming_log['URI'].tolist()
+
+streaming_log = streaming_log.drop(columns=['URI'], axis=1)
+streaming_log.to_excel(f'{output_path}spotify_streaming_log.xlsx', index=False)
+
+print('Minutes Played: {:,}'.format(filtered_data['ms_played'].sum() // 60000))
+
 count = 0
-countList = [0]
-indexList = []
-uniqueUriList = []
-for i in range(len(uriList) - 1):
-	if (uriList[i] == uriList[i+1]):
+count_list = [0]
+index_list = []
+unique_uri_list = []
+
+for i in range(len(uri_list) - 1):
+	if (uri_list[i] == uri_list[i+1]):
 		count = count + 1
-		indexList.append(i)
-		uniqueUriList.append(uriList[i])
-		countList.append(count)
+		index_list.append(i)
+		unique_uri_list.append(uri_list[i])
+		count_list.append(count)
 	else:
-		countList.append(1)
+		count_list.append(1)
 		count = 0
-mpi = (countList.index(max(countList)))
-print('On', tsList[mpi - max(countList)], 'you started playing', trackList[mpi] + '.',
-	'After playing it', max(countList), 'times in a row, you stopped playing it at', tsList[mpi])
+mpi = (count_list.index(max(count_list)))
+print('On', ts_list[mpi - max(count_list)], 'you started playing', track_list[mpi] + '.', 'After playing it', max(count_list), 'times in a row, you stopped playing it at', ts_list[mpi])
 
-topArtistsDf = pd.DataFrame(columns = ['Rank', 'Artist', 'Streams'])
-topArtists = []
-artistList = data['Artist'].tolist()
-for i in range(100): #change to see different number of top artists
-	def most_frequent(artistList):
-		return max(set(artistList), key = artistList.count)
-	topArtist = most_frequent(artistList)
-	topArtistsDf = topArtistsDf.append({'Rank': i+1, 'Artist': topArtist, 'Streams': artistList.count(topArtist)}, ignore_index = True)
-	topArtists.append(topArtist)
-	artistList = [i for i in artistList if i != topArtist]
-display(topArtistsDf.to_string(index = False))
-topArtistsDf.to_excel(f'{outputPath}/spotify_top_artists.xlsx', index = False)
+track_artist_df = pd.DataFrame(list(zip(
+	filtered_data['master_metadata_track_name'], 
+	filtered_data['master_metadata_album_artist_name'],
+	filtered_data['spotify_track_uri'])),
+	columns=['Track', 'Artist', 'URI']
+)
 
-topAlbumsDf = pd.DataFrame(columns = ['Rank', 'Album', 'Streams'])
-topAlbums = []
-albumList = data['Album'].tolist()
-for i in range(50): #change to see different number of top albums
-	def most_frequent(albumList):
-		return max(set(albumList), key = albumList.count)
-	topAlbum = most_frequent(albumList)
-	topAlbumsDf = topAlbumsDf.append({'Rank': i+1, 'Album': topAlbum, 'Streams': albumList.count(topAlbum)}, ignore_index = True)
-	topAlbums.append(topAlbum)
-	albumList = [i for i in albumList if i != topAlbum]
-display(topAlbumsDf.to_string(index = False))
-topAlbumsDf.to_excel(f'{outputPath}/spotify_top_albums.xlsx', index = False)
+track_artist_df = (
+	track_artist_df[~track_artist_df[['Track', 'Artist', 'URI']]
+	.apply(lambda x: 'None' in x.values, axis=1)]
+	.reset_index(drop=True)
+)
 
-print('URI List Length:', len(uriList))
-print('Starting URI Mapping: %s seconds' % round(time.time() - startTimerSec, 2))
-artistList = data['Artist'].tolist()
-for i in range(len(uriList)):
-	for j in range(len(uriList)):
-		if((trackList[i] == trackList[j]) 
-			and (artistList[i] == artistList[j])
-			and (uriList[i] != uriList[j])):
-			uriList[i] = uriList[j]
-matchedTrackUriDf = pd.DataFrame(list(zip(trackList, artistList, uriList)), columns = ['Track', 'Artist', 'URI'])
-uniqueTrackUriDf = matchedTrackUriDf.groupby(['URI'], as_index = False).first()
-done_uri_mapping = datetime.now()
-done_uri_mapping = done_uri_mapping.strftime('%H:%M:%S')
-print('Done URI Mapping:', done_uri_mapping)
-print('Done URI Mapping: %s seconds' % round(time.time() - startTimerSec, 2))
+track_artist_df['URI'] = track_artist_df['URI'].str.replace('spotify:track:', '')
 
-uniqueTrackList = uniqueTrackUriDf['Track'].tolist()
-uniqueArtistList = uniqueTrackUriDf['Artist'].tolist()
-uniqueUriList = uniqueTrackUriDf['URI'].tolist()
-uriList = matchedTrackUriDf['URI'].tolist()
-rank = []
-topTracks = []
-topArtistsFromTracks = []
-streamsFromTracks = []
-topUris = []
-for i in range (100): #change to see different number of top tracks
-#for i in range (len(uniqueUriList)): #use this to get streams for every unique song streamed
-	def most_frequent(uriList):
-		return max(set(uriList), key = uriList.count)
-	topUriIntermediate = most_frequent(uriList)
-	for j in range(len(uniqueUriList)):
-		if (topUriIntermediate == uniqueUriList[j]):
-			topTrackIntermediate = (uniqueTrackList	[j])
-			break
-	for k in range(len(uniqueArtistList)):
-		if (topUriIntermediate == uniqueUriList[k]):
-			topTrackArtistIntermediate = (uniqueArtistList[k])
-			break
-	rank.append(i+1)
-	topTracks.append(topTrackIntermediate)
-	topArtistsFromTracks.append(topTrackArtistIntermediate)
-	streamsFromTracks.append(uriList.count(topUriIntermediate))
-	topUris.append(topUriIntermediate)
-	uriList = [i for i in uriList if i != topUriIntermediate]
-topTracksDf = pd.DataFrame(list(zip(rank, topTracks, topArtistsFromTracks, streamsFromTracks, topUris)), columns = ['Rank', 'Track', 'Artist', 'Streams', 'URI'])
-display(topTracksDf.to_string(index = False))
-topTracksDf.to_excel(f'{outputPath}/spotify_top_tracks.xlsx', index = False)
+artist_counts = track_artist_df['Artist'].value_counts()
+top_artists_df = pd.DataFrame({'Artist': artist_counts.index, 'Streams': artist_counts.values})
 
-end_time = datetime.now()
-end_time = end_time.strftime('%H:%M:%S')
-print('Start Time:', start_time)
-print('End Time:', end_time)
+# print top 100 tracks
+rows_to_print = 100
+# print ALL tracks
+# rows_to_print = top_artists_df.shape[0]
+pd.set_option('display.max_rows', None)
+top_artists = top_artists_df.head(rows_to_print).reset_index().rename(columns={'index': 'Rank'})
+top_artists['Rank'] += 1
+print(top_artists.to_string(index=False))
+
+top_artists.to_excel(f'{output_path}top_artists.xlsx', index=False)
+
+track_artist_df['Duplicate URI'] = track_artist_df['URI']
+groups = track_artist_df.groupby(['Track', 'Artist'])
+
+for group_name, group_df in groups:
+	if len(group_df) > 1:
+		unique_uris = group_df['URI'].unique()
+		track_artist_df.loc[group_df.index, 'Duplicate URI'] = unique_uris[0]
+
+track_artist_df['URI'] = track_artist_df['Duplicate URI']
+track_artist_df.drop('Duplicate URI', axis=1, inplace=True)
+
+count_df = track_artist_df['URI'].value_counts().reset_index()
+grouped_df = track_artist_df.groupby('URI').agg({'Track': lambda x: ', '.join(x.unique()), 'Artist': lambda x: ', '.join(x.unique())}).reset_index()
+merged_df = count_df.rename(columns={'URI': 'Streams', 'index': 'URI'}).merge(grouped_df, on='URI')
+merged_df = merged_df[['URI', 'Track', 'Artist', 'Streams']]
+merged_df = merged_df.drop('URI', axis=1)
+
+# print top 100 tracks
+rows_to_print = 100
+# print ALL tracks
+# rows_to_print = merged_df.shape[0]
+pd.set_option('display.max_rows', None)
+top_tracks = merged_df.head(rows_to_print).reset_index().rename(columns={'index': 'Rank'})
+top_tracks['Rank'] += 1
+print(top_tracks.to_string(index=False))
+top_tracks.to_excel(f'{output_path}top_tracks.xlsx', index=False)
